096c02d5af8abc649013befc4dd4e14f428bc2e1
Kevin Le <kevin@phobos.com.au>
2024-08-09 15:53:22 +0800

    logging and rate limiting

--- api.py
+++ api.py
@@ -1,3 +1,4 @@
+import logging
 import json
 import os
 from flask import Flask, jsonify, make_response, request
@@ -10,9 +11,11 @@ app = Flask(__name__)
 redis = Redis(url=os.getenv("KV_REST_API_URL"), token=os.getenv("KV_REST_API_TOKEN"))
 ratelimit = Ratelimit(
     redis=redis,
-    limiter=FixedWindow(max_requests=5, window=10),
+    limiter=FixedWindow(max_requests=10, window=10),
     prefix="@upstash/paris-ratelimit",
 )
+
+logger = logging.getLogger(__name__)
 
 
 @app.route("/", methods=["GET"])
@@ -27,10 +30,13 @@ def home():
 @app.route("/medals", methods=["GET"])
 def get_medal_tally():
     ip = request.headers.get("X-Real-Ip")
+    logger.info(f"Request from {ip}")
+
     ioc_noc_code = request.args.get("country")
 
     ratelimit_gate = ratelimit.limit(ip)
     if not ratelimit_gate.allowed:
+        logger.warning(f"Rate limit exceeded for {ip}")
         response = make_response(jsonify({"error": "Rate limit exceeded"}))
         response.status_code = 429
         response.headers["Retry-After"] = 10
@@ -40,7 +46,9 @@ def get_medal_tally():
     cached_response = redis.get(cache_key)
 
     if cached_response:
-        return make_response(jsonify(json.loads(cached_response)), 200)
+        response = make_response(jsonify(json.loads(cached_response)))
+        response.headers["Access-Control-Allow-Origin"] = "*"
+        return response
 
     results = get_olympic_medal_tally(ioc_noc_code=ioc_noc_code)
     response = make_response(jsonify(results))
