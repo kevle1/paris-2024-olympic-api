6ee354ef7af80804a6b9a79c17ce9040e0a79488
Kevin Le <git@kevle.xyz>
2024-08-18 13:30:30 +0800

    add final results

--- api/api.py
+++ api/api.py
@@ -17,15 +17,14 @@ def home():
 
 @app.route("/medals", methods=["GET"])
 def get_medal_tally():
-    ip = request.headers.get('X-Real-Ip')
-    print(ip)
-
     ioc_noc_code = request.args.get("country")
     results = get_olympic_medal_tally(ioc_noc_code=ioc_noc_code)
+
     response = make_response(jsonify(results))
-    # https://vercel.com/docs/edge-network/caching
     response.headers["Access-Control-Allow-Origin"] = "*"
+    # https://vercel.com/docs/edge-network/caching
     response.headers["Cache-Control"] = "public, s-maxage=1800"
+
     return response
 
 
